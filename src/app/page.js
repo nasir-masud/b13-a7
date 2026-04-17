3083e7947a380f29c3958c094f4a5f877a9b330a
nasir-masud <cactas.28@gmail.com>
2026-04-17 14:06:17 +0600

    Correction banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import BannerStats from "./components/BannerStats";
-import FriendSection from "./components/FriendSection";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className='container mx-auto'>
       <Hero />
-      <BannerStats />
-      <FriendSection />
     </div>
   );
 }
